9f565800009d9b93de7e2da3e995b86125a07535
Takashi Mori <kanetugu2015@gmail.com>
2025-11-27 05:25:36 +0900

    add button

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -18,8 +18,42 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 console.log("[HakoniwaViewer] Map initialized");
 
-// PDUマネージャ初期化
-const websocketCommunicationService = new WebSocketCommunicationService(CONFIG.wire_version);
-const pduManager = new PduManager({ wire_version: CONFIG.wire_version });
-await pduManager.initialize(CONFIG.pdu_def_path, websocketCommunicationService);
-console.log("[HakoniwaViewer] PduManager initialized");
+
+// PDUマネージャ初期化関数
+async function initializePduManager() {
+    // PDUマネージャ初期化
+    const websocketCommunicationService = new WebSocketCommunicationService(CONFIG.ws_uri, CONFIG.wire_version);
+    const pduManager = new PduManager({ wire_version: CONFIG.wire_version });
+    await pduManager.initialize(CONFIG.pdu_def_path, websocketCommunicationService);
+    console.log("[HakoniwaViewer] PduManager initialized");
+    return pduManager;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const connectBtn = document.getElementById('connect-btn');
+    let isConnected = false;
+    let pduManager = null;
+
+    connectBtn.addEventListener('click', async () => {
+        if (!isConnected) {
+            try {
+                pduManager = await initializePduManager();
+                if (pduManager) {
+                    console.log("[HakoniwaViewer] Connected.");
+                    connectBtn.textContent = 'disconnect';
+                    isConnected = true;
+                }
+            } catch (error) {
+                console.error("[HakoniwaViewer] Connection failed:", error);
+            }
+        } else {
+            if (pduManager && pduManager.getCommunicationService()) {
+                //TODO pduManager.getCommunicationService().close();
+                console.log("[HakoniwaViewer] Disconnected.");
+            }
+            pduManager = null;
+            connectBtn.textContent = 'connect';
+            isConnected = false;
+        }
+    });
+});
