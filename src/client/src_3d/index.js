3c77df39285433a81f27a570507612bc7120d0b9
Takashi Mori <kanetugu2015@gmail.com>
2025-12-03 13:17:50 +0900

    refactored

--- src/client/src_3d/index.js
+++
@@ -1,2 +0,0 @@
-export { PduManager } from '/thirdparty/hakoniwa-pdu-javascript/src/PduManager.js';
-export { WebSocketCommunicationService } from '/thirdparty/hakoniwa-pdu-javascript/src/impl/WebSocketCommunicationService.js';
